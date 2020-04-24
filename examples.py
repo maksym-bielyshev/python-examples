7bf8a6aabe190f46d69fe254aefec73714c90884
maksym-bielyshev <maksym.bielyshev@gmail.com>
2020-04-24 11:51:56 +0300

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,4 +1,52 @@
 # What is the output of this code?
+class Myclass:
+    n = 0
+
+    def change(self, val):
+        n = val
+obj = Myclass()
+obj.change(1)
+print(obj.n)
+
+# Output: 0
+
+
+# What is the output of this code?
+print('y', end="=")
+x, y = 7, 9
+y *= x
+y %= 8
+print(y)
+
+# Output: y=7
+
+
+# What is the output of this code?
+lst = [34, 67, 34]
+if lst.sort() == sorted(lst):
+    print('true')
+else:
+    print('false')
+
+# Output: false
+
+
+# What is the output of this code?
+
+
+def fun(x):
+    if x == 1:
+        return 1
+    else:
+        return x * fun(x - 1)
+print(fun(5))
+
+# Output: 120
+
+
+# What is the output of this code?
+
+
 def meth():
     g = [2, 3, 4, 5, 7]
     for i in g:
@@ -24,8 +72,6 @@ foo()
 
 
 # What is the output of this code?
-
-
 d = {'A': 3, 'B': 8}
 for n in range(2, 12, 2):
     d['A'] += n
